8093c8eb905510606ae5c9aa5672f442f99a19f8
Franciscot93 <franciscot.p.93@gmail.com>
2023-07-24 22:58:01 -0300

    añado estructura para las pages, y una carpeta para los componentes, creo el router y establesco el router provider en el main.jsx

---
+++ MiMascotaTuc/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+function Layout() {
+  return (
+    <div className="md:flex md:min-h-screen">
+        <aside className="w-screen text-center font-semibold text-slate-200 text-6xl
+        e shadow-md mb-10 rounded-lg  border-slate-100"></aside>
+        <main></main>
+        <footer></footer>
+    </div>
+  )
+}
+
+export default Layout
--- MiMascotaTuc/src/main.jsx
+++ MiMascotaTuc/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import './index.css'
+import Layout from './components/Layout'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
