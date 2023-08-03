6d630a689f935a58cfb97ad6972bccbff03ca071
AngelFaciano <126618958+AngelFaciano@users.noreply.github.com>
2023-08-02 23:47:22 -0300

    actualizando contenindo

--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useLogin } from '../store/userZustand'
-
-export const ProtectedRoute = ({ children }) => {
-  const { isLogged } = useLogin()
-  return isLogged ? children : <Navigate to="/" replace={true} />
-}
