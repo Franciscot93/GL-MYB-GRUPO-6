d027b4b9de519344bfb8bf55c2e24e6689f3bbfa
agustin <agustin_correanz@outlook.com>
2023-08-04 11:39:30 -0300

    Realice comentarios descriptivos en los archivos - src/data/usuarios.jsx - src/hook/useForm.js - src/index.css - src/store/userZustand.js - Para poder facilitar la lectura del codigo que estamos trabajando con el equipo

--- MiMascotaTuc/src/data/usuarios.jsx
+++ MiMascotaTuc/src/data/usuarios.jsx
@@ -1,24 +1,21 @@
 export async function obtenerUsuarios(){
     
+  // Esta funcion asincronica obtiene una lista de usuarios desde la API.
     const respuesta = await fetch(import.meta.env.VITE_API_URL)
     const resultado = await respuesta.json()
-
-    
     return resultado
-   
 }
 
-
-
+// Esta funcion asincronica obitene los datos de un usuario especifico basado en su ID.
 export async function obtenerDatosUsuario(id){
     const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${id}`)
     const resultado= await respuesta.json()
-   
     return resultado
 }
 
+// Esta funcion asincronica agrega un nuevo usuario a la API.
 export async function agregarUsuario(datos){
- try{
+ try {
     const respuesta= await fetch(import.meta.env.VITE_API_URL,{
         method: 'POST',
         body: JSON.stringify(datos),
@@ -27,10 +24,10 @@ export async function agregarUsuario(datos){
         }
     })
     await respuesta.json()
- }catch{console.log(error)}}
-
+ } catch (error) {console.log(error)}}
+// Esta funcion asincronica edita los datos de un usuario existente en la API.
  export async function editarUsuario(id,datos){
-    try{
+    try {
        const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${id}`,{
            method: 'POST',
            body: JSON.stringify(datos),
@@ -39,26 +36,26 @@ export async function agregarUsuario(datos){
            }
        })
        await respuesta.json()
-    }catch{console.log(error)}}
+    }catch (error) {console.log(error)}}
 
 
-
+     // Esta funcion asincronica elimina un usuario de la API segun su ID.
     export async function eliminarCliente(id){
-        try{
+        try {
            const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${id}`,{
                method: 'DELETE'
-               
            })
            await respuesta.json()
-        }catch{console.log(error)}}
+        }catch (error) {console.log(error)}}
 
 
 
 //MASCOTAS
 
 
-
+// Esta funcion guada una mascota para un usuario existente en la API.
  export const guardarMascotas = async(userData,agregarMascota) => {
+    // Crea un nuevo objeto de usuario con la mascota agregada.
     const usuario={
         id: userData.id,
       username: userData.username,
@@ -68,9 +65,11 @@ export async function agregarUsuario(datos){
       Imagen: "",
       mascotas: [...userData.mascotas,agregarMascota],
     }
+      console.log(usuario)
 
-    console.log(usuario)
-   try{const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
+   try {
+    // Realiza una solicitud PUT para actualizar los datos del usuario con la nueva mascota.
+    const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
       method: "PUT",
       body: JSON.stringify(usuario),
       headers: {
@@ -79,14 +78,10 @@ export async function agregarUsuario(datos){
      
     });
     await respuesta.json()
-  }catch{console.log(error)}}
+  }catch (error) {console.log(error)}
+}
 
- 
-  
-    
-    
-
-
+ // Funcion que genera un ID unico combinando la fecha actual y un valor aleatorio.
     export const generarId = () => {
         const date = Date.now().toString(36);
         const random = Math.random().toString(36).substring(2);
@@ -94,11 +89,8 @@ export async function agregarUsuario(datos){
         return date + random;
       }
 
-
-
-   
+      // Esta funcion elimina una mascota especifica de las mascotas de un usuario en la API.
       export const eliminarMascotas = async(userData,idMascota) => {
-        
         const mascotaEliminada=userData.mascotas.filter(mascota=> mascota.id!==idMascota)
         const usuario={
             id: userData.id,
@@ -109,21 +101,22 @@ export async function agregarUsuario(datos){
           Imagen: "",
           mascotas: mascotaEliminada,
         }
-
         console.log(usuario)
-       try{const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
+
+       try {
+        const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
           method: "PUT",
           body: JSON.stringify(usuario),
           headers: {
             "Content-Type": "application/json",
           }
-         
         });
         await respuesta.json()
-      }catch{console.log(error)}}
-    
-      export const editarMascotas = async(userData,idMascota) => {
-        
+      }catch (error) {console.log(error);
+      }
+    }
+     // Esta funcion edita los datos de una mascota especifica de las mascotas de un usuario en la API.
+      export const editarMascotas = async (userData,idMascota) => {
         const mascotaEliminada=userData.mascotas.filter(mascota=> mascota.id!==idMascota)
         const usuario={
             id: userData.id,
@@ -134,9 +127,9 @@ export async function agregarUsuario(datos){
           Imagen: "",
           mascotas: mascotaEliminada,
         }
-
-        console.log(usuario)
-       try{const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
+          console.log(usuario)
+       try {
+        const respuesta= await fetch(`${import.meta.env.VITE_API_URL}/${usuario.id}`, {
           method: "PUT",
           body: JSON.stringify(usuario),
           headers: {
@@ -145,5 +138,5 @@ export async function agregarUsuario(datos){
          
         });
         await respuesta.json()
-      }catch{console.log(error)}}
+      }catch (error) {console.log(error)}}
     
--- MiMascotaTuc/src/store/userZustand.js
+++ MiMascotaTuc/src/store/userZustand.js
@@ -1,10 +1,17 @@
 import { create } from 'zustand'
 
+// Crea un store de Zustand para administrar el estado relacionado con el inicio de sesion de usuarios.
 export const useLogin = create((set) => ({
+  // Estado inicial del store.
   isLogged: false,
+  // Funcion para realizar el inicio de sesion y cambiar el estado "isLogged" a 'true'.
   login: () => set({ isLogged: true }),
+  // funciion para cerrar sesion y cambiar el estado 'isLogged' a 'false'
   logout: () => set({ isLogged: false }),
+  // Almacena los datos del usuario.
   user:[],
+
+  // Funcion para actualzar los datos del usuario en el estado del store.
   setUser:(usuario)=>set({user:usuario}),
   
 }))
