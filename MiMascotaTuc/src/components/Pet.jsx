8b8ed5f36f4d72c29551b3e0550833c61d83baf2
Franciscot93 <franciscot.p.93@gmail.com>
2023-08-07 17:44:34 -0300

    cambios en en el funcionamiento y apariencia del navbar, modificacion del estilo del componente pet, se agrega funcion de setteo de mascota en el useLogin de zustand

--- MiMascotaTuc/src/components/Pet.jsx
+++ MiMascotaTuc/src/components/Pet.jsx
@@ -6,7 +6,7 @@ import { Image } from "cloudinary-react";
 function Pet({ pet }) {
   console.log(pet);
   // Obteniendo el usuario actual desde el estado global
-  const { user, setUser, eliminarMascota } = useLogin();
+  const { user, setUser, eliminarMascota,setPet } = useLogin();
   // Iniciar la funcion de navegacion
   const navigate = useNavigate();
   // Manejo de la eliminacion de una mascota
@@ -21,7 +21,7 @@ function Pet({ pet }) {
     <div className="relative overflow-hidden flex bg-cover aspect-square justify-center content-center place-content-center place-items-center align-middle text-cent bg-no-repeat">
     <Image  cloudName='dqr2aiayz' publicId={pet.pic}/>
     </div>
-    <div className="px-2 text-left flex flex-row flex-wrap">
+    <div className=" text-left flex shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] rounded-md flex-row flex-wrap">
       <div className="flex-1 w-4/5">
     <p className="font-semibold  mb-1 hover:scale-105 text-gray-700 uppercase">
       Nombre: {""}
@@ -47,13 +47,14 @@ function Pet({ pet }) {
           </Link></span>
         </p>
     </div>
-      
-    </div>
     <div className="flex flex-wrap content-center container place-items-center place-content-center  md:justify-between mt-4 mb-1 box-border ">
           <Link to={`/usuario/perfilDelUsuario/${user.id}/editarMascota/${pet.id}`} className="flex  flex-wrap transition-colors logoTitle m-auto text-2xl text-center text-indigo-600 hover:text-indigo-700">Editar</Link>
           <Link onClick={handleEliminar} className="flex transition-colors flex-wrap m-auto logoTitle   text-red-600 hover:text-red-700 text-2xl text-center">Eliminar
           </Link>
         </div>
+      
+    </div>
+    
   </div>
  
   )
