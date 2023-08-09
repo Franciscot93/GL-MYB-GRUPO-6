1c94a48cc0c3dbc6e0b915f7b04e0bb39cc08b49
agustin <agustin_correanz@outlook.com>
2023-08-09 08:29:33 -0300

    Realice cambios permanentes en src/components/Card.jsx - src/components/Footer.jsx - src/pages/About.jsx - agregue las fotos para el footer de los logos de tw fb ig y elimine las img de Facebook Twitter y meta, en footer cambie la ruta de importaciones de dichas imagenes y en donde se implementaban

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,14 +2,19 @@ import image from '../img/MiMascotaTuc.jpeg'
 
 const About = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${image})` }}>
-      <div className="max-w-md p-6 bg-white rounded shadow-md transition-transform hover:transform hover:scale-150 hover:border-neon-gold">
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${image})`, filter: 'grayscale(100%)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    '@media (max-width: 768px)': {
+      backgroundSize: 'contain',
+    }
+    }}>
+      <div className="max-w-md p-6 bg-white rounded shadow-md transition-transform hover:transform hover:scale-150 ">
         <h2 className="text-2xl font-semibold mb-8">Acerca de MiMascotaTuc</h2>
         <p className="text-gray-600">
           ¡Bienvenido a MiMascotaTuc! Somos un equipo apasionado que se dedica a facilitar
           el cuidado de los animales a través de nuestra aplicación.
         </p>
-        <div className="h-6 w-6 absolute animate-spin rounded-full border-t-4 border-neon-blue border-opacity-35"></div>
       </div>
     </div>
   );
